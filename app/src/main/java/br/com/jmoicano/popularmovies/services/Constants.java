bbede233ad49c3ea67dea1ec9ca32e88dc1db48b
Moicano <joao.silva@meupag.com.br>
2019-05-14 17:01:04 -0300

    Constants
    Class to group the constants used

---
+++ app/src/main/java/br/com/jmoicano/popularmovies/services/Constants.java
@@ -0,0 +1,11 @@
+package br.com.jmoicano.popularmovies.services;
+
+public final class Constants {
+    public static final String SCHEME = "https";
+    public static final String BASE_URL = "api.themoviedb.org";
+    public static final String VERSION = "3/";
+    public static final String API_KEY = "api_key";
+    public static final String SRV_MSG_ERROR = "An error occoured in the service. Try again later";
+    public static final String CONNECTION_MSG_ERROR = "Connection problems.";
+    public static final String GENERIC_MSG_ERROR = "Something went wrong :'(";
+}
