2ae6a47803bb04af11c9fb31d9f08c4282aa9a60
Moicano <joao.silva@meupag.com.br>
2019-05-14 16:59:28 -0300

    Refactor
    Refactor da classe MainActivity

--- app/src/main/java/br/com/jmoicano/popularmovies/main/ui/view/MainActivity.kt
+++ app/src/main/java/br/com/jmoicano/popularmovies/main/ui/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package br.com.jmoicano.popularmovies
+package br.com.jmoicano.popularmovies.main.ui.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import br.com.jmoicano.popularmovies.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
