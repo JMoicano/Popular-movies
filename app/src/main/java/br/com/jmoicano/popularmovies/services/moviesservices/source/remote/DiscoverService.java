00c55d0d15397ba6534aa4bdc9778a81a333e6b4
Moicano <joao.silva@meupag.com.br>
2019-05-17 19:13:13 -0300

    gitflow-hotfix-stash: endpoint
    Fixing endpoints to call

--- app/src/main/java/br/com/jmoicano/popularmovies/services/moviesservices/source/remote/DiscoverService.java
+++ app/src/main/java/br/com/jmoicano/popularmovies/services/moviesservices/source/remote/DiscoverService.java
@@ -3,10 +3,10 @@ package br.com.jmoicano.popularmovies.services.moviesservices.source.remote;
 import br.com.jmoicano.popularmovies.services.moviesmodels.MovieDiscoverResponseModel;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 public interface DiscoverService {
 
-    @GET("/3/discover/movie")
-    Call<MovieDiscoverResponseModel> getMovies(@Query("sort_by") String sort);
+    @GET("/3/movie/{sort}")
+    Call<MovieDiscoverResponseModel> getMovies(@Path("sort") String sort);
 }
